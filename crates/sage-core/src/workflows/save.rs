41fbb02d02d06fdd723b49612c65225bd4a41335
crazywolf132 <crazywolf132@gmail.com>
2025-05-08 16:21:55 +1000

    feat(save): uplifted to use sage_tui for confirmation prompt

--- crates/sage-core/src/workflows/save.rs
+++ crates/sage-core/src/workflows/save.rs
@@ -186,13 +186,9 @@ fn stage_correct_files(opts: &SaveOpts) -> Result<()> {
     }
 
     // User has both staged and unstaged/untracked changes.
-    if staged_changes && unstaged_changes {
+    if staged_changes && (unstaged_changes || untracked_files) {
         println!("⚠️  You have mixed changes");
-        println!("Do you want to stage all changes? (y/n)");
-        let mut answer = String::new();
-        std::io::stdin().read_line(&mut answer)?;
-        let answer = answer.trim();
-        if answer.eq_ignore_ascii_case("y") {
+        if sage_tui::basic::confirm(String::from("Do you want to stage all changes? (y/n)"))? {
             stage_all()?;
             println!("●   Staged all changes ✔");
         } else {
--- crates/sage-tui/src/basic/mod.rs
+++ crates/sage-tui/src/basic/mod.rs
@@ -1,3 +1,5 @@
+pub mod confirm;
 pub mod select;
 
+pub use confirm::confirm;
 pub use select::select;
