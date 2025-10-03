036aae79bcbbadc59f6bc014628ab12eb6e39732
binbandit <crazywolf132@gmail.com>
2025-10-03 17:40:56 +1000

    refactor(sage-git): init repo cache on open

--- crates/sage-git/src/lib.rs
+++ crates/sage-git/src/lib.rs
@@ -17,7 +17,9 @@ pub struct Repo {
 
 impl Repo {
     pub fn open() -> Result<Self> {
-        let repo = gix::discover(".")?;
+        let mut repo = gix::discover(".")?;
+        // Set object cache if not already set (16 MB _*should*_ be safe enough)
+        repo.object_cache_size_if_unset(16 * 1024 * 1024);
         Ok(Self { repo })
     }
 
